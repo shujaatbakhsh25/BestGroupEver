e1c7be826a360f623952d8f25bfcfb90114dd541
bluefinch83 <37155961+bluefinch83@users.noreply.github.com>
2019-12-28 13:04:57 -0500

    Will long Watermark edits
    I changed the functions so that they accept arguments.

--- Watermark.py
+++ Watermark.py
@@ -1,14 +1,14 @@
 import os
 import csv
 import shutil
-from Add_WatermarkV3 import *
+from BestGroupEver.Add_WatermarkV3 import *
 
 
-def createUserList():
+def createUserList(user_path, user_file):
     """ Creates a list of tuples with username and code from csv file """
     users = []
 
-    with open(os.path.join(os.getcwd(), 'src', 'users.csv')) as infile:
+    with open(os.path.join(os.getcwd(), user_path, user_file)) as infile:
         reader = csv.reader(infile, delimiter=',')
         next(reader, None)
         for i in reader:
@@ -16,39 +16,44 @@ def createUserList():
     return users
 
 
-def makeUserDirectories():
+def makeUserDirectories(user_path, user_file, dest_path):
     """ Makes user directories in the dest folder. 
     Deletes present directories and creates new ones."""
     try:
-        shutil.rmtree(os.path.join(os.getcwd(), 'dest'))
+        shutil.rmtree(os.path.join(os.getcwd(), dest_path))
     except Exception as e:
         print(e)
         pass
-    os.mkdir(os.path.join(os.getcwd(), 'dest'))
-    users = createUserList()
+    os.mkdir(os.path.join(os.getcwd(), dest_path))
+    users = createUserList(user_path, user_file)
     for i in users:
         try:
-            os.mkdir(os.path.join(os.getcwd(), 'dest', f'{i[0]}'))
+            os.mkdir(os.path.join(os.getcwd(), dest_path, f'{i[0]}'))
         except FileExistsError as e:
             pass
 
 
-def watermark():
+def watermark(user_path, user_file, dest_path, pic_path, pic_type, alp, method, n):
     """ Watermarks all the images in src folder for each user.
     Puts the watermarked image in the dest folder under each user's directory. """
-    users = createUserList()
-    for i in [f for f in os.listdir(os.path.join(os.getcwd(), 'src')) if f.endswith('.png')]:
-        image_path = os.path.join(os.getcwd(), 'src', i)
+    users = createUserList(user_path, user_file)
+    for i in [f for f in os.listdir(os.path.join(os.getcwd(), pic_path)) if f.endswith(pic_type)]:
+        image_path = os.path.join(os.getcwd(), pic_path, i)
         seed = list(map(lambda x: x[1], users))
-        alp = 0.5
-        method = 'haar'
-        n = 3
         image_wm = add_watermark(image_path, seed, alp, method, n)
         for j in range(len(image_wm)):
-            cv2.imwrite(f'dest/{users[j][0]}/{i.split(".")[0]}.png', image_wm[j])
+            cv2.imwrite(f'{dest_path}/{users[j][0]}/{i.split(".")[0]}{pic_type}', image_wm[j])
 
 
 if __name__ == "__main__":
-    createUserList()
-    makeUserDirectories()
-    watermark()
+    user_path = 'BestGroupEver/src'
+    user_file = 'users.csv'
+    dest_path = 'BestGroupEver/dest'
+    pic_path = 'BestGroupEver/src'
+    pic_type = '.png'
+    alp = 0.5
+    method = 'haar'
+    n = 3
+    createUserList(user_path, user_file)
+    makeUserDirectories(user_path, user_file, dest_path)
+    watermark(user_path, user_file, dest_path, pic_path, pic_type, alp, method, n)
